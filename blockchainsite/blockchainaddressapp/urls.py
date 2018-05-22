385e9ee84c23ee58c3f14053bd7784782aa9c0fa
DarkAlek <aleksy@mistal.net>
2018-05-22 12:23:16 +0200

    first-final-stage

--- blockchainsite/blockchainaddressapp/urls.py
+++ blockchainsite/blockchainaddressapp/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<slug:address_id>/', views.AddressView.as_view(), name='address')
+    #path('<slug:address_id>/', views.AddressView.as_view(), name='address'),
+    re_path(r'^(?P<address_id>[0-9a-zA-Z]{27,34})/$', views.AddressView.as_view(), name='address'),
+    path('qrcode/', views.QrCodeView.as_view(), name='qrcode')
 ]
--- blockchainsite/blockchainaddressapp/views.py
+++ blockchainsite/blockchainaddressapp/views.py
@@ -2,10 +2,14 @@ import json
 import urllib3
 import datetime
 import pytz
+import base64
+import qrcode
+from io import BytesIO
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import ListView
+from django.db.models import Sum
 from .models import Address, Transaction
 
 # Create your views here.
@@ -20,13 +24,48 @@ class AddressView(ListView):
     context_object_name = 'transactions'
     model = Transaction
 
+    def get_context_data(self, **kwargs):
+        context = super(AddressView, self).get_context_data(**kwargs)
+        if context['transactions'] is not None:
+            context['count'] = context['transactions'].count()
+            context['sum'] = context['transactions'].aggregate(Sum('value'))['value__sum']
+            positive_sum = context['transactions'].filter(value__gt=0).aggregate(Sum('value'))['value__sum']
+            negative_sum = context['transactions'].filter(value__lt=0).aggregate(Sum('value'))['value__sum']
+            print(positive_sum, negative_sum)
+            if positive_sum is not None and negative_sum is not None:
+                context['total_value'] = positive_sum - negative_sum
+            elif positive_sum is not None:
+                context['total_value'] = positive_sum
+            elif negative_sum is not None:
+                context['total_value'] = -negative_sum
+            else:
+                context['total_value'] = 0
+            return context
+
     def get_queryset(self):
-        address_param = self.kwargs['address_id']  
+        address_param = self.kwargs['address_id']
+        datetime_start = None
+        datetime_end = None
+        start_date = self.request.GET.get('start', None)
+        end_date = self.request.GET.get('end', None)
+        if start_date is not None:
+            try:
+                datetime_start = datetime.date(*(int(x) for x in start_date.split('-')))
+            except ValueError:
+                datetime_start = None
+        if end_date is not None:
+            try:
+                datetime_end = datetime.date(*(int(x) for x in end_date.split('-'))) + datetime.timedelta(days=1)
+            except ValueError:
+                datetime_end = None
+        #print(start_date.split('-'), end_date.split('-'))
         queryset = Transaction.objects.filter(address__address=address_param)
         if len(queryset) != 0:
+            queryset = self.filter_date(queryset, datetime_start, datetime_end)
             return queryset
         else:
-            queryset = self.get_transactions(self.kwargs['address_id'])
+            queryset = self.get_transactions(address_param)
+            queryset = self.filter_date(queryset, datetime_start, datetime_end)
             return queryset
 
     def get_transactions(self, address):
@@ -73,3 +112,29 @@ class AddressView(ListView):
             amount = sum([x['value'] for x in out if x['value'] != 0 and x['addr'] == address])
             value = amount   
         return value
+    
+    def filter_date(self, queryset, start_date, end_date):
+        if start_date is not None or end_date is not None:
+            if start_date is not None and end_date is not None:
+                return queryset.filter(date__range=[start_date, end_date])
+            elif start_date is not None:
+                return queryset.filter(date__gte=start_date)
+            else:
+                return queryset.filter(date__lte=end_date)
+        else:
+            return queryset
+        
+
+class QrCodeView(TemplateView):
+    template_name = "qrcode.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(QrCodeView, self).get_context_data(**kwargs)
+        address = self.request.GET.get('address', None)
+        if address is not None:
+            img = qrcode.make(address)
+            buffered = BytesIO()
+            img.save(buffered, format="JPEG")
+            img_str = base64.b64encode(buffered.getvalue())
+            context['qrcode'] =  img_str.decode('utf-8')
+            return context
